e066050d74da135f7a45805a8b62f3c9fec0a1fa
444171810@qq.com <sherlock.ma@perficient.com>
2023-06-26 18:01:59 +0800

    add license info

--- 斗鱼去广告.js
+++ 斗鱼去广告.js
@@ -7,6 +7,7 @@
 // @match        https://www.douyu.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
+// @license      MIT
 // ==/UserScript==
 
 (function () {
