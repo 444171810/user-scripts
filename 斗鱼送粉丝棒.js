5f925a6d1af0945ab68fe6ca0b38340ec4b78dc4
444171810@qq.com <sherlock.ma@perficient.com>
2023-07-18 14:33:06 +0800

    update description

--- 斗鱼送粉丝棒.js
+++ 斗鱼送粉丝棒.js
@@ -2,7 +2,7 @@
 // @name         斗鱼送粉丝棒
 // @namespace    http://tampermonkey.net/
 // @version      0.2
-// @description  try to take over the world!
+// @description  斗鱼一键送出粉丝棒，用于续粉丝牌
 // @author       Sherlock
 // @match        https://www.douyu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=douyu.com
